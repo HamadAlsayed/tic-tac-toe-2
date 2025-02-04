170a77bf1b193572669c38b56d24fd94962c64fa
HamadAlsayed <hamad_sayed@outlook.com>
2025-02-04 13:08:07 +0200

    Create 2D array board to represent the game board

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const Gameboard = {
+function Gameboard() {
+    const board = [];
+    const rows = 3;
+    const columns = 3;
 
+    // Populate board with arrays to turn it into a 2D array
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push('cell');
+        }
+    }
+    
+    
+
+    return {
+        
+
+    }
 }
 
 function playerFactory(playerOneName = 'Player One', playerTwoName = 'Player Two') {
@@ -7,5 +24,7 @@ function playerFactory(playerOneName = 'Player One', playerTwoName = 'Player Two
 }
 
 function gameController() {
-    
+
 }
+
+let game = Gameboard();
